e4fa972da4f2fbe069f01571617ca635618fe3f0
Transcraft99 <aries1749@live.fr>
2016-07-27 01:45:18 +0200

    commeencmeent du game

--- deeplib_s.js
+++ deeplib_s.js
@@ -1,6 +1,20 @@
 //LIB SERVEUR
 
 
+var game = [];
+    
+var searchGame = function(idRoom){
+    var result = -1;
+    for(var i = 0; i < game.length; i++)
+    {
+        if(game[i].id == idRoom)
+        {
+            result = i;
+        }
+    }
+    return result;  
+};
+
 
 function serv(){
     
@@ -17,10 +31,26 @@ function serv(){
         this.arg2 = arg2;
         
         this.start = function(app, arg1, arg2){
-            app.get(arg1, function(req, res){
-                res.render(arg2);
-                res.end();
-            });
+            if(arg1.indexOf(':idR') > -1)
+                {
+                    app.get(arg1, function(req, res){
+                        var nbGame = searchGame(req.params.idR);
+                        if(nbGame == -1)
+                        {
+                            res.send("EXISTE PAS DSL");
+                        }
+                        else{
+                            res.render(arg2);
+                        }
+                        res.end();
+                    });
+                }
+            else{
+                app.get(arg1, function(req, res){
+                    res.render(arg2);
+                    res.end();
+                });
+            }
         }
     }
     
